0e0cfb3ef84438bc068f3407be582181f5153351
ilix <me@ilix.se>
2015-05-20 19:08:50 +0200

    Trying to fix usings and make the code a bit more simple so we can get it working...

--- Controllers/FaceController.cs
+++ Controllers/FaceController.cs
@@ -1,7 +1,10 @@
-using System.Threading.Tasks;
 using System.Dynamic;
 using System.IO;
 using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
 
 namespace FaceProxy.Web
 {
@@ -71,18 +74,11 @@ namespace FaceProxy.Web
         /// <param name="analyzesGender">If set to <c>true</c> [analyzes gender].</param>
         /// <param name="analyzesHeadPose">If set to <c>true</c> [analyzes head pose].</param>
         /// <returns>The detected faces.</returns>
-        public async string Detect(string url, bool analyzesFaceLandmarks = false, bool analyzesAge = false, bool analyzesGender = false, bool analyzesHeadPose = false)
+        
+        /*
+        public void Detect(string url, bool analyzesFaceLandmarks = false, bool analyzesAge = false, bool analyzesGender = false, bool analyzesHeadPose = false)
         {
-            var requestUrl = string.Format(
-                "{0}/{1}?analyzesFaceLandmarks={2}&analyzesAge={3}&analyzesGender={4}&analyzesHeadPose={5}&{6}={7}",
-                ServiceHost,
-                DetectionsQuery,
-                analyzesFaceLandmarks,
-                analyzesAge,
-                analyzesGender,
-                analyzesHeadPose,
-                SubscriptionKeyName,
-                this.subscriptionKey);
+            var requestUrl = RequestUrl();
                 
             using(var wc = new System.Net.WebClient())
             {
@@ -93,22 +89,50 @@ namespace FaceProxy.Web
                 return response;
             }
         }
+        */
+        
+        public string RequestUrl(bool analyzesFaceLandmarks = false, bool analyzesAge = false, bool analyzesGender = false, bool analyzesHeadPose = false) {
+            return string.Format(
+                "{0}/{1}?analyzesFaceLandmarks={2}&analyzesAge={3}&analyzesGender={4}&analyzesHeadPose={5}&{6}={7}",
+                ServiceHost,
+                DetectionsQuery,
+                analyzesFaceLandmarks,
+                analyzesAge,
+                analyzesGender,
+                analyzesHeadPose,
+                SubscriptionKeyName,
+                this.subscriptionKey);
+        }
 
     }
 
     public class FaceController : Controller
     {
         [HttpPost]
-        public JsonResult Index(DynamicObject tweet)
+        public JsonResult Index(string url)
         {
             // TODO:
             // Parse json
             // Get image url from tweet: tweet.media_url
             // Send the image url to Face API with this key
             var apiKey = "74847df195954443bea84965b272a072";
+            var faceApi = new FaceApi();
 
+// OMG THIS EDITOR DOES NOT INDENT PROPERLY AFTER INSERTING A LINE BREAK
+
+            var requestUrl = faceApi.RequestUrl();
+                
+            using(var wc = new System.Net.WebClient())
+            {
+
+                dynamic requestBody = new ExpandoObject();
+                requestBody.url = url;
+                var response = wc.UploadString(requestUrl, Json(requestBody));
+                return response;
+            }
+        
             // adjust/parse the response?
-            var response = FaceApi.Detect(tweet.media_url as string);
+            //var response = FaceApi.Detect(tweet.media_url as string);
 
             // send it back as json 
             return Json(response);        
